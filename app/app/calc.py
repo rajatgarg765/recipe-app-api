0ab9bebe7c76a51fdc6a7e803e7ab2b1053066d3
rajatgarg765 <rajatrj349@gmail.com>
2022-12-25 20:56:18 +0530

    added tests from section 7

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Function
+"""
+
+def add(x,y):
+    """Add x and y and return"""
+    return x+y
+
+def subtract(x,y):
+    """subtract """
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Example Tests
+"""
+
+from django.test import SimpleTestCase
+from app.calc import add, subtract
+
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_number(self):
+        """adding here"""
+        res=add(5,9)
+        
+        self.assertEqual(res,14)
+    
+    def test_subtract_number(self):
+        """subtract here"""
+        res=subtract(10,5)
+
+        self.assertEqual(res,5)
